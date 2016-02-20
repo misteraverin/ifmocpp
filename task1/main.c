cbea207970715d5d852328d134caa2c0808c8235
Maxim Averin <amax0703@gmail.com>
2016-02-20 12:55:22 +0300

    Fix scanf

--- task1/main.c
+++ task1/main.c
@@ -18,12 +18,15 @@ int main (void)
       a[i][j] = i * j;
   
   int x1, y1, x2, y2;
-  while(scanf("%d %d %d %d", &x1, &y1, &x2, &y2) > 0  && x1 != 0){
+  scanf("%d", &x1);
+  while(x1 != 0){
+     scanf("%d %d %d", &y1, &x2, &y2);
      for(int i = x1; i <= x2; i++){
       for(int j = y1; j <= y2; j++)
-        printf("%d ", a[i][j]);
+        printf("%4d", a[i][j]);
       printf("\n");
      }   
+     scanf("%d", &x1);
   }
   for(int i = 0; i < n; i++) 
      free(*(a + i));
