fd759a5f0b54bc6367a0e7403c2b22f27efab5aa
Maxim Averin <amax0703@gmail.com>
2016-05-20 23:32:11 +0300

    first release

--- task2/main.c
+++ task2/main.c
@@ -1,10 +1,3 @@
-/*
-    TO-DO
-    2) get_str
-    3) change
-    5) find
-
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,7 +21,8 @@ database telephone_book;
 const char *file_name;
 int global_id = 0;
 
-int find_substr(char *a, char *b);
+int  find_substr(char *a, char *b);
+int  is_good(char c, int type);
 void change();
 void create(FILE * file, int num);
 void delete(int id);
@@ -38,17 +32,115 @@ char *get_str(FILE * file, int type);
 
 
 void change(){
+  int num;
+  int type_oper;
+  char command[30];
+  char * cur_str;
+  scanf("%d %s", &num, command);
+  if(!strcmp(command, "number"))
+    type_oper = 1;
+  else
+    type_oper = 0;
+  cur_str = get_str(stdin, type_oper);
+  if(cur_str != NULL){
+    int cur_id = -1;
+    int i = 0;
+    for(i = 0; i < telephone_book.size; i++)
+        if(telephone_book.records[i].id == num){
+            cur_id = i;
+            break;
+        }
+    if(cur_id == -1){
+      printf("Error: wrong id\n");
+      free(cur_str);
+    }
+    else{
+        if(type_oper == 1)
+          telephone_book.records[cur_id].telephone_number = cur_str;
+        else
+          telephone_book.records[cur_id].name = cur_str;
+    }
+    rewrite_file();
+  }
+  else{
+    printf("Error: wrong command\n");
+    free(cur_str);
+  }
   return;
 }
 
 
 void find(){
+  char *query = get_str(stdin, 2);
+  if(query != NULL){
+    int flag = 0;
+    int i = 0;
+    for(i = 0; i < telephone_book.size; i++){
+      int expr =  (isdigit(query[0]) && strcmp(telephone_book.records[i].telephone_number, query) == 0);
+      int expr1 = (isalpha(query[0]) && find_substr(telephone_book.records[i].name, query));
+      if(expr || expr1){
+        flag = 1;
+        printf("%d %s %s\n", telephone_book.records[i].id, telephone_book.records[i].name, telephone_book.records[i].telephone_number);
+      }
+    }
+    if(!flag)
+     printf("Error: can't find this contact\n");
+  }
+  else{
+   printf("Error\n");
+  }
+  free(query);
   return;
 }
 
 
+int is_good(char c, int type) {
+  int mas[3];
+  mas[0] = isalpha(c);
+  mas[1] = isdigit(c);
+  mas[2] = isalnum(c);
+  return mas[type];
+}
+
 char *get_str(FILE * file, int type){
-  return NULL;
+    // 0 - name, 1 - number, 2 - mixed string
+    char *right_string = NULL;
+    int bad = 0;
+    int len = 0;
+    int size = 0;
+    char symb = fgetc(file);
+    while(symb != EOF){
+        if(symb == ' '){
+            if(type == 2){
+                if(isalpha(symb))
+                  type = 0;
+                else
+                  type = 1;
+            }
+            if(is_good(symb, type)){
+                if( (len + 1) == size){
+                    size = size * 2;
+                    right_string = realloc(right_string, sizeof(char) * size);
+                }
+                right_string[len++] = symb;
+                right_string[len] = '\0';
+
+            }
+            else if((symb == '+' && len > 0) ||
+                    (type == 0) || type != 0 && symb != '(' && symb != ')' && symb != '-' && symb != '+'){
+                bad = 1;
+            }
+        }
+        else if(len > 0)
+          break;
+        symb = fgetc(file);
+    }
+    if(bad){
+        free(right_string);
+        right_string = NULL;
+        printf("Error: wrong string\n");
+    }
+    return right_string;
 }
 
 
